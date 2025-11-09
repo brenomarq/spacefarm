de7aa2a4b846c2cd191e7261a26af2fc8335986d
Breno Marques <brenomarques516@gmail.com>
2025-11-09 14:53:57 -0300

    :boom: fix: reseting the project

--- backend/src/models/parameters.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class AvailableParameters(str, Enum):
-    """Parâmetros disponíveis para a geração de mapas de calor."""
-    UMIDADE = "umidade"
-    TEMPERATURA = "temperatura"
-    NPK = "npk"
-    PH = "ph"
